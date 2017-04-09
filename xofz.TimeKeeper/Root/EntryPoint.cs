8c2990b8a1c3fa4e345a9cd1382886886321e290
Sam Pearson <sam@xofz.com>
2017-04-08 20:39:05 -0700

    Make sure to embed assemblies into the executable and load them

--- xofz.TimeKeeper/Root/EntryPoint.cs
+++ xofz.TimeKeeper/Root/EntryPoint.cs
@@ -1,13 +1,15 @@
 ﻿namespace xofz.TimeKeeper.Root
 {
     using System;
+    using System.Reflection;
     using System.Windows.Forms;
 
-    static class EntryPoint
+    internal static class EntryPoint
     {
         [STAThread]
-        static void Main()
+        private static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, e) => loadEmbeddedAssembly(e.Name);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
@@ -15,6 +17,30 @@
             bootstrapper.Bootstrap();
 
             Application.Run(bootstrapper.MainForm);
+        }
+
+        private static Assembly loadEmbeddedAssembly(string name)
+        {
+            var assemblyName = new AssemblyName(name);
+            if (name.EndsWith("Retargetable=Yes"))
+            {
+                return Assembly.Load(assemblyName);
+            }
+
+            var container = Assembly.GetExecutingAssembly();
+            var path = assemblyName.Name + ".dll";
+
+            using (var stream = container.GetManifestResourceStream(path))
+            {
+                if (stream == null)
+                {
+                    return null;
+                }
+
+                var bytes = new byte[stream.Length];
+                stream.Read(bytes, 0, bytes.Length);
+                return Assembly.Load(bytes);
+            }
         }
     }
 }
